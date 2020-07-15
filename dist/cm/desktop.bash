7f9d91109c046e8d4abcc3d11477a480b7e6a8e5
yuniel <yuniel.work@gmail.com>
2020-07-15 18:37:09 -0400

    cm: add pkgs

--- dist/cm/desktop.bash
+++ dist/cm/desktop.bash
@@ -1,6 +1,23 @@
 
 # TODO: agregar paqutes de apps
 
+# base
 dotf-i bashc
-# dotf-i google-chrome
+
+echoc 'Package Managers'
+dotf-i brew
+# :base
+
+echoc 'Web apps'
 dotf-i webapps
+
+echoc 'Internet'
+dotf-i google-chrome
+
+echoc 'Text editors'
+dotf-i typora
+
+echoc 'Development'
+dotf-i vscode
+dotf-i postman
+dotf-i insomnia
