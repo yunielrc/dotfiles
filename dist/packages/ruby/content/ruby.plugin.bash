0093580de6b5096a0a3c4c54af902f6be5e9ab60
yuniel <yuniel.work@gmail.com>
2020-08-01 22:21:05 -0400

    chore: aliases

--- dist/packages/ruby/content/ruby.plugin.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if which ruby >/dev/null && which gem >/dev/null; then
-    PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-    PATH="$(ruby -r rubygems -e 'puts Gem.dir')/bin:$PATH"
-fi
--- dist/packages/ruby/test/setup.bats
+++
@@ -1,7 +0,0 @@
-load test_helper
-
-@test 'should install ruby & config' {
-  bash ../setup
-  type -P ruby
-  type -P gem
-}
