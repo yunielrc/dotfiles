fd9540b7e330ea075741661067f43c0738293764
yuniel <yuniel.work@gmail.com>
2020-08-16 18:05:03 -0400

    add aws cli bashc plugin

---
+++ dist/packages/aws-cli/content/aws-cli.plugin.bash
@@ -0,0 +1,7 @@
+# Aliases
+
+# EC2
+alias ec2-stop="aws ec2 stop-instances --instance-ids"
+alias ec2-start="aws ec2 start-instances --instance-ids"
+alias ec2-status="aws ec2 describe-instance-status"
+alias ec2-describe="aws ec2 describe-instances"
