8490da684ca78680e0cad214e73d2a470785a047
yuniel <yuniel.work@gmail.com>
2020-08-22 15:31:01 -0400

    test: create bashc gen and plugins folders

--- dist/test/test_helper_base.bash
+++ dist/test/test_helper_base.bash
@@ -7,6 +7,10 @@ test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 export DEBUG=true
-export PATH="${PATH}:~/.local/bin"
+export PATH="${PATH}:${HOME_BIN}"
+
+[[ ! -d ~/.bashc ]] && {
+  mkdir -p ~/.bashc/{gen,plugins/local}
+}
 
 cd "$BATS_TEST_DIRNAME" || exit
