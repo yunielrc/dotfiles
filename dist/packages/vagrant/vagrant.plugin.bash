d80cb779bdf79d2b43492d889eac1ac00c10aade
yuniel <yuniel.work@gmail.com>
2020-09-11 00:03:17 -0400

    feat: add vagrant plugin

---
+++ dist/packages/vagrant/vagrant.plugin.bash
@@ -0,0 +1,9 @@
+# Aliases
+
+alias va='vagrant'
+alias vau='vagrant up'
+alias vassh='vagrant ssh'
+alias vah='vagrant halt'
+alias vad='vagrant destroy -f'
+alias vaupload='vagrant upload'
+alias vas='vagrant status'
