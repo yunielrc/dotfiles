364b4c3e5223f15da3678661ae20d6719956aee9
yuniel <yuniel.work@gmail.com>
2020-09-17 15:29:54 -0400

    add vagrant aliases

--- dist/packages/vagrant/vagrant.plugin.bash
+++ dist/packages/vagrant/vagrant.plugin.bash
@@ -2,9 +2,9 @@
 
 alias va='vagrant'
 alias vau='vagrant up'
-alias vassh='vagrant ssh'
+alias vas='vagrant ssh'
 alias vah='vagrant halt'
 alias vad='vagrant destroy -f'
 alias vaupload='vagrant upload'
-alias vas='vagrant status'
+alias vast='vagrant status'
 alias vap='vagrant push'
