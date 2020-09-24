106abd086810c5a52c774207e472d62128a47133
yuniel <yuniel.work@gmail.com>
2020-09-24 13:27:42 -0400

    updates fzf command

--- dist/packages/fzf/fzf.plugin.bash
+++ dist/packages/fzf/fzf.plugin.bash
@@ -13,10 +13,24 @@ fi
 source "/home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.bash"
 
 # Environment
-# export FZF_DEFAULT_COMMAND='fd --hidden --follow --no-ignore --exclude .git'
-export FZF_DEFAULT_COMMAND='find'
-export FZF_COMPLETION_TRIGGER='**'
+export FZF_DEFAULT_COMMAND="command ${FD_COMMAND}"
+# export FZF_DEFAULT_COMMAND="command ${FIND_COMMAND}"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_COMPLETION_TRIGGER=',,'
 export FZF_DEFAULT_OPTS='--multi --height 50% --layout=reverse --border'
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  $FZF_DEFAULT_COMMAND . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  $FZF_DEFAULT_COMMAND --type d . "$1"
+}
 
 # Supported commands
 # usage: _fzf_setup_completion path|dir|var|alias|host COMMANDS...
